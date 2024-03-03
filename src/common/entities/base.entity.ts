4b9a7abd168429997347abdaba7e460888906ff2
Hyunho Son <aa000098@naver.com>
2024-03-03 17:00:56 +0900

    feat: env key 파일 및 base모델 추가

---
+++ src/common/entities/base.entity.ts
@@ -0,0 +1,12 @@
+import { CreateDateColumn, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export abstract class BaseModel {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
