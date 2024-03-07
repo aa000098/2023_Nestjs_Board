d536bf5cb1b8bb983fcbc74ab88bedd522bd1b66
Hyunho Son <aa000098@naver.com>
2024-03-07 17:00:02 +0900

    fix: role 데코레이터 authority 데코레이터로 이름 변경

--- src/space/role/decorator/authority.decorator.ts
+++ src/space/role/decorator/authority.decorator.ts
@@ -3,4 +3,4 @@ import { RoleEnum } from "../const/role.const";
 
 export const ROLE_KEY = 'user_role';
 
-export const Role = (...roles:RoleEnum[]) => SetMetadata(ROLE_KEY, roles);
+export const Authority = (...roles:RoleEnum[]) => SetMetadata(ROLE_KEY, roles);
