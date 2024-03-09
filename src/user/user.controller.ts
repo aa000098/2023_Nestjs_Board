427247eb16fb065861fff588122b40c8140a2241
Hyunho Son <aa000098@naver.com>
2024-03-09 11:50:20 +0900

    fix: user controller 로그인해야 접근 가능하도록 변경

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,18 @@
-import { Controller, Delete, Get, Param, ParseIntPipe, Req, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserModel } from './entities/user.entity';
 import { User } from './decorator/user.decorator';
-import { IsPublic } from 'src/common/decorator/is-public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  @IsPublic()
   getUsers() {
     return this.userService.getAllUsers();
   }
 
   @Get(':userId')
-  @IsPublic()
   getUser(
     @User() user: UserModel,
     @Param('userId', ParseIntPipe) userId: number,
